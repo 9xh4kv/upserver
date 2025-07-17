5d7b95bb9f7adaa315836cc90e8b61e828ad9471
9xh4kv <ouzak0402@gmail.com>
2025-07-17 19:30:50 -0300

    Download feature added

--- upserver.py
+++ upserver.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import ssl
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory, abort, url_for
 
 app = Flask(__name__)
 
@@ -9,23 +9,56 @@ app = Flask(__name__)
 cur_dir = os.getcwd()
 
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/<path:subpath>', methods=['GET', 'POST'])
+def upload_file(subpath=''):
+    message = None
+    current_path = os.path.join(cur_dir, subpath)
+    
+    if os.path.isfile(current_path):
+        return download_file(subpath)
+    
     if request.method == 'POST':
         files = request.files.getlist('file')  # Retrieve multiple files
         uploaded_files = 0 
         for file in files:
             if file:
                 # Save the uploaded file to the upload folder
-                filename = os.path.join(cur_dir, file.filename)
+                filename = os.path.join(current_path, file.filename)
                 file.save(filename)
                 uploaded_files += 1  # Increment the counter for each uploaded file
                 print("Receiving file: " + filename)
         if uploaded_files > 0:
-            return f'{uploaded_files} file(s) uploaded successfully!\n'
+            message = f'{uploaded_files} file(s) uploaded successfully!\n'
         else:
-            return 'No files were uploaded.'
-    return render_template('upload.html')
+            message = 'No files were uploaded.'
+    
+    items = []
+    try:
+        for item in os.listdir(current_path):
+            item_path = os.path.join(subpath, item)
+            if os.path.isdir(os.path.join(current_path, item)):
+                items.append((item, 'folder'))
+            else:
+                items.append((item, 'file'))
+    except FileNotFoundError:
+        abort(404)  # Return a 404 error without traceback
+    
+    return render_template('upload.html', message=message, items=items, subpath=subpath)
 
+@app.route('/<path:filename>')
+def download_file(filename):
+    try:
+        return send_from_directory(cur_dir, filename, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
+
+@app.route('/<path:subpath>')
+def navigate(subpath):
+    current_path = os.path.join(cur_dir, subpath)
+    if os.path.isdir(current_path):
+        return upload_file(subpath)
+    else:
+        return download_file(subpath)
 
 def main():
     parser = argparse.ArgumentParser(description='Run the upserver application.')
@@ -47,7 +80,7 @@ def main():
         app.run(host='0.0.0.0', port=args.port, ssl_context=context)
     else:
         # Run the app without SSL
-        print("HTPPS \033[31mdisabled\033[0m, running server \033[31mwithout encryption\033[0m.")
+        print("HTTPS \033[31mdisabled\033[0m, running server \033[31mwithout encryption\033[0m.")
         print("==================================================")
         app.run(host='0.0.0.0', port=args.port)
 
